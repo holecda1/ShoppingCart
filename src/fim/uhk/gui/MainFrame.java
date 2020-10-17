3e950469dfcbaf4c71ca9e3985291a9f567a1dc8
holecda1 <holecda1@uhk.cz>
2020-10-17 16:49:10 +0200

    ShoppingCart - metoda getTotalPrice Main - otestovani metody getTotalPrice

--- src/fim/uhk/gui/MainFrame.java
+++ src/fim/uhk/gui/MainFrame.java
@@ -1,20 +1,78 @@
 package fim.uhk.gui;
 
+import fim.uhk.ShoppingCartItem;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainFrame extends JFrame {
-    public MainFrame(){
-        initFrame() ;
-        initGui();
-    }
+public class MainFrame extends JFrame implements ActionListener {
 
-    public void initFrame() {
-        setTitle("PRO2 - Shopping cart");
-        setSize(800,500);
+    JButton btnInputAdd;
+    JButton btnInputRemove;
+    JTextField txtInputName;
+    JTextField txtInputPricePerPiece;
+    JTextField txtInputPieces;
+
+
+    public MainFrame(int width, int height) {
+        super("PRO2 - Shopping cart");
+        setSize(width, height);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-    }
-    public void initGui() {
+        initGui();
 
     }
+
+    public void initGui() {
+        JPanel panelMain = new JPanel(new BorderLayout());
+
+        JPanel panelInput = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel panelTable = new JPanel();
+        JPanel panelFooter = new JPanel();
+
+        JLabel lblInputName = new JLabel("Název:");
+        txtInputName = new JTextField("", 15);
+
+        JLabel lblInputPricePerPiece = new JLabel("Cena/kus");
+        txtInputPricePerPiece = new JTextField("", 5);
+
+        JLabel lblInputPieces = new JLabel("Počet kusů:");
+        txtInputPieces = new JTextField("", 5);
+
+        btnInputAdd = new JButton("Přijat");
+        btnInputAdd.addActionListener(this);
+        btnInputRemove = new JButton("Smazat");
+        btnInputRemove.addActionListener(this);
+
+        txtInputName.setText(String.valueOf(txtInputName));
+
+        panelInput.add(lblInputName);
+        panelInput.add(txtInputName);
+        panelInput.add(lblInputPricePerPiece);
+        panelInput.add(txtInputPricePerPiece);
+        panelInput.add(lblInputPieces);
+        panelInput.add(txtInputPieces);
+
+        panelInput.add(btnInputAdd);
+
+
+        panelMain.add(panelInput, BorderLayout.NORTH);
+        panelMain.add(panelTable, BorderLayout.CENTER);
+        panelMain.add(panelFooter, BorderLayout.SOUTH);
+
+        add(panelMain);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == btnInputAdd) {
+            System.out.println("Přidán produkt:");
+        } else if (actionEvent.getSource() == btnInputRemove) {
+            System.out.println("Kliknuto na smazat");
+        }
+    }
+
+
 }
+//
